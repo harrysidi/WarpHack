19268507971835ac946689f6027d8c15949cb325
Harry Sidiropoulos <harry.sidi@gmail.com>
2025-07-29 09:31:40 +0200

    on the way there

--- game/huntandkill.go
+++ game/huntandkill.go
@@ -6,28 +6,58 @@ import (
 )
 
 type cell struct {
-	x, y    int
-	visited bool
+	x,y int
 }
 
 // HuntAndKill implements the Hunt and Kill maze generation algorithm.
 func HuntAndKill(dungeonSeed uint64, width int, height int) *DungeonLevel {
 
-	maze := [][]cell{}
+	maze := [][]bool{}
 	for y := range height {
 		row := []cell{}
 		for x := range width {
-			row = append(row, cell{x: x, y: y, visited: false})
+			row = append(row, false)
 		}
 		maze = append(maze, row)
 	}
 	fmt.Print("Generating maze with seed: ", dungeonSeed, "\n")
+	fmt.Println(maze)
+
 	rng := rand.New(rand.NewPCG(dungeonSeed, uint64(dungeonSeed/2+1)))
 	// Start with a random cell
 	startX := rng.IntN(width)
 	startY := rng.IntN(height)
-	maze[startY][startX].visited = true
-
+	maze[startY][startX]= true
+	currentX := startX
+	currentY := startY
 	//kill phase
+	for {
+		unvisited := []cell{}
+		wallToRemove := []cell{}
+		if x+2 < width && !maze[y][x+2] {
+			unvisited = append(unvisited, cell{y: y, x: x + 2})
+			wallToRemove = append(wallToRemove, cell{y: y, x: x + 1})
+		}
+		if x-2 >= 0 && !maze[y][x-2]{
+			unvisited = append(unvisited, cell{y: y, x: x - 2})
+			wallToRemove = append(wallToRemove, cell{y: y, x: x - 1})
+		}
+		if y+2 < height && !maze[y+2][x] {
+			unvisited = append(unvisited, cell{y: y + 2, x: x})
+			wallToRemove = append(wallToRemove, cell{y: y + 1, x: x})
+		
+		}
+		if y-2 >= 0 && !maze[y-2][x] {
+			unvisited = append(unvisited, cell{y: y - 2, x: x})
+			wallToRemove = append(wallToRemove, cell{y: y - 1, x: x})
+		}
+		
+		if len(unvisited) >0 {
+			index:= rng.IntN(len(unvisited))
+			remove_wall := unvisited[rng.IntN(len(unvisited))]
+
+			DungeonLevel[]
+x
+	}
 
 }
